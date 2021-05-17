7bd24a25a7afc73253a43a04e606448ef43f365f
NicoNicoNii <84341703+Niconisia@users.noreply.github.com>
2021-05-17 15:00:01 -0300

    Update Principal.java

--- src/main/java/com/daniloarantes/primeiroprojeto/Principal.java
+++ src/main/java/com/daniloarantes/primeiroprojeto/Principal.java
@@ -14,5 +14,6 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("Olá Mundo!");
         System.out.println("Olá Danilo");
+        //Faça elevar, o cosmo no seu coração, todo mal, combater, despertarrrrr o poder.
     }
 }
