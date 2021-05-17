d3d0d52635fd8436e3f83931ab96d90f1410f141
Danilo Arantes <daniloarantes@gmail.com>
2021-05-17 14:42:27 -0300

    Adicionado Olá Mundo

--- src/main/java/com/daniloarantes/primeiroprojeto/Principal.java
+++ src/main/java/com/daniloarantes/primeiroprojeto/Principal.java
@@ -12,6 +12,6 @@ package com.daniloarantes.primeiroprojeto;
 public class Principal {
     // Método principal
     public static void main(String[] args) {
-        
+        System.out.println("Olá Mundo!");
     }
 }
